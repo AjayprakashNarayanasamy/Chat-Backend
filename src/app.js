b05a8a11787123f14aaa65c52834a3eea57c0fe9
AjayPrakash06 <ajay.prakash@agilisium.com>
2026-08-30 05:13:34 +0530

    Initial Setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('Ajay')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  }),
+);
+
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.status(200).json({
+    msg: 'Chat Application',
+  });
+});
+
+module.exports = app;
